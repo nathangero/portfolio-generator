0783ddfc4ffb647e77cced6f1277016d7bb6e7dd
nathangero <nathangero@yahoo.com>
2023-10-19 13:00:08 -0700

    implement rendering html from user's response

--- index.js
+++ index.js
@@ -28,9 +28,41 @@ function askUser() {
         ])
         .then((response) => {
             const { name, location, linkedin, github, ...others } = response
-            console.log(name, location, linkedin, github, others)
 
+            fs.writeFile("index.html", renderHtml(response), (err) => {
+                err ? console.log(err) : console.log("Created porfolio html")
+            })
         })
+}
+
+
+function renderHtml({ name, location, linkedin, github, ...others }) {
+    console.log(name, location, linkedin, github, others)
+    let githubUrl = `https://github.com/${github}`
+    
+    let htmlStr = `<!DOCTYPE html>` +
+    `<html lang="en">` +
+    `<head>` +
+    `   <meta charset="UTF-8">` +
+    `   <meta name="viewport" content="width=device-width, initial-scale=1.0">` +
+
+
+    `   <title>My Porfolio</title>` +
+    `</head>` +
+    `<body>` +
+    `   <header>` +
+    `       <h1>My name is ${name}</h1>` +
+    `       <h2>Located in ${location}</h2>` +
+    `   </header>` +
+    `   <main>` +
+    `       <p>My LinkedIn profile is: <a target="_blank" href="${linkedin}">${linkedin}</a></p>` +
+    `       <p>My GitHub profile is: <a target="_blank" href="${githubUrl}">${githubUrl}</a></p>` +
+    `   </main>` +
+    `</body>` +
+    `</html>`
+
+    
+    return htmlStr
 }
 
 askUser();
