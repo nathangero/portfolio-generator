d4ffbd187d3d5a1c448d2125c77e30aa10ad5d5f
nathangero <nathangero@yahoo.com>
2023-10-19 13:17:15 -0700

    implement renderHtml() with wireframe styling

--- index.js
+++ index.js
@@ -40,24 +40,45 @@ function renderHtml({ name, location, linkedin, github, ...others }) {
     console.log(name, location, linkedin, github, others)
     let githubUrl = `https://github.com/${github}`
     
-    let htmlStr = `<!DOCTYPE html>` +
-    `<html lang="en">` +
-    `<head>` +
-    `   <meta charset="UTF-8">` +
-    `   <meta name="viewport" content="width=device-width, initial-scale=1.0">` +
-
-
-    `   <title>My Porfolio</title>` +
-    `</head>` +
-    `<body>` +
-    `   <header>` +
-    `       <h1>My name is ${name}</h1>` +
-    `       <h2>Located in ${location}</h2>` +
-    `   </header>` +
-    `   <main>` +
-    `       <p>My LinkedIn profile is: <a target="_blank" href="${linkedin}">${linkedin}</a></p>` +
-    `       <p>My GitHub profile is: <a target="_blank" href="${githubUrl}">${githubUrl}</a></p>` +
-    `   </main>` +
+    let htmlStr = `<!DOCTYPE html>\n` +
+    `<html lang="en">\n` +
+    `<head>\n` +
+    `   <meta charset="UTF-8">\n` +
+    `   <meta name="viewport" content="width=device-width, initial-scale=1.0">\n` +
+    `   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">\n` + 
+    `   <title>My Porfolio</title>\n` +
+    `</head>\n` +
+    `<body>\n` +
+    `   <div class="container">\n` +
+    `       <div class="jumbotron">\n` +
+    `           <div class="jumbotron">\n` +
+    `               <h1 class="display-4">I'm ${name}</h1>\n` +
+    `               <p class="lead">I'm located in ${location}</p>\n` +
+    `               <hr class="my-4">\n` +
+    `               <table class="table table-bordered">\n` +
+    `                   <thead>\n` +
+    `                       <tr>\n` +
+    `                           <th scope="col">Profiles</th>\n` +
+    `                           <th scope="col">Links</th>\n` +
+    `                       </tr>\n` +
+    `                   </thead>\n` +
+    `                   <tbody>\n` +
+    `                       <tr>\n` +
+    `                           <td>LinkedIn</td>\n` +
+    `                           <td id="linkedin-link"><a target="_blank" href="${linkedin}">${linkedin}</a></td>\n` +
+    `                       </tr>\n` +
+    `                       <tr>\n` +
+    `                           <td>GitHub</td>\n` +
+    `                           <td id="github-link"><a target="_blank" href="${githubUrl}">${githubUrl}</a></td>\n` +
+    `                       </tr>\n` +
+    `                   </body>\n` +
+    `               </table>\n` +
+    `           </div>\n` +
+    `       </div>\n` +
+    `   </div>\n` +
+    `<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>\n
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>\n
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>\n` +
     `</body>` +
     `</html>`
 
